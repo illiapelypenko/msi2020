c876eaf8ba180b9f0f5f471ece3c2e75696ebc4b
IlliaPelypenko <35105041+illiapelypenko@users.noreply.github.com>
2020-05-07 16:19:08 +0300

    delete extra stuff

--- src/components/JokeFinder/Categories.js
+++ src/components/JokeFinder/Categories.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './Categories.scss';
 
 const Categories = ({ onCategoryChange, categories, selectedCategory }) => {
--- src/components/JokeFinder/FormGroup.js
+++ src/components/JokeFinder/FormGroup.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const FormGroup = ({ groupValue, currentValue, text, onChange }) => {
 	return (
